7c256c8fda1a655fd23a8cb886e448d354e63cdf
ricknijhuis <40138649+ricknijhuis@users.noreply.github.com>
2025-03-07 14:16:30 +0100

    fix callback types

--- src/events/events.zig
+++ src/events/events.zig
@@ -9,14 +9,28 @@ pub fn Events(EventT: type) type {
     return struct {
         const Self = @This();
 
-        pub fn Callback(ContextT: type) type {
-            return struct {
-                context: ContextT,
-                callback: *const fn (ctx: ContextT, event: *const EventT) bool,
-            };
-        }
+        pub const Callback = struct {
+            ptr: *anyopaque,
+            callback: *const fn (ptr: *anyopaque, event: *const EventT) bool,
 
-        callbacks: ArrayList(Callback(*anyopaque)),
+            pub fn init(ptr: anytype, comptime callbackFn: fn (@TypeOf(ptr), *const EventT) bool) Callback {
+                const T = @TypeOf(ptr);
+
+                const gen = struct {
+                    pub fn callback(pointer: *anyopaque, event: *const EventT) bool {
+                        const self: T = @ptrCast(@alignCast(pointer));
+                        return callbackFn(self, event);
+                    }
+                };
+
+                return .{
+                    .ptr = ptr,
+                    .callback = gen.callback,
+                };
+            }
+        };
+
+        callbacks: ArrayList(Callback),
 
         pub fn init(allocator: Allocator, size: usize) !Self {
             return .{
@@ -32,24 +46,21 @@ pub fn Events(EventT: type) type {
         /// If a non null context is passed, that context will be send to the callback together with the event.
         pub fn register(
             self: *Self,
-            context: anytype,
-            comptime callbackFn: fn (@TypeOf(context), *const EventT) bool,
+            ptr: anytype,
+            comptime callbackFn: fn (@TypeOf(ptr), *const EventT) bool,
         ) void {
-            std.debug.assert(@TypeOf(context) != @TypeOf(null));
+            std.debug.assert(@TypeOf(ptr) != @TypeOf(null));
 
-            self.callbacks.appendAssumeCapacity(.{
-                .context = @alignCast(@ptrCast(context)),
-                .callback = @ptrCast(&callbackFn),
-            });
+            self.callbacks.appendAssumeCapacity(.init(ptr, callbackFn));
         }
 
         pub fn unregister(
             self: *Self,
-            context: anytype,
-            comptime callbackFn: fn (@TypeOf(context), *const EventT) bool,
-        ) ?@TypeOf(context) {
+            ptr: anytype,
+            comptime callbackFn: fn (@TypeOf(ptr), *const EventT) bool,
+        ) ?@TypeOf(ptr) {
             for (self.callbacks.items, 0..) |callback, i| {
-                if (callback.context == @as(@TypeOf(callback.context), @alignCast(@ptrCast(context))) and
+                if (callback.ptr == @as(@TypeOf(callback.ptr), @alignCast(@ptrCast(ptr))) and
                     callback.callback == @as(@TypeOf(callback.callback), @ptrCast(&callbackFn)))
                 {
                     return @alignCast(@ptrCast(self.callbacks.swapRemove(i).context));
@@ -60,7 +71,7 @@ pub fn Events(EventT: type) type {
 
         pub fn fire(self: *const Self, event: *const EventT) bool {
             for (self.callbacks.items) |callback| {
-                if (callback.callback(callback.context, event)) {
+                if (callback.callback(callback.ptr, event)) {
                     return true;
                 }
             }
@@ -83,7 +94,7 @@ test "Events: if callback returns true, fire returns true" {
         const Self = @This();
         bar: i32,
         //
-        pub fn handleEvent(self: *Self, event: *const Event) bool {
+        pub fn handleEvent(self: *@This(), event: *const Event) bool {
             self.bar += event.foo;
             return true;
         }
@@ -214,4 +225,30 @@ test "Events: multiple callback context types can be registered" {
     _ = events.fire(&.{ .foo = 1 });
     try testing.expectEqual(1, callback.bar);
     try testing.expectEqual(2, callback1.bar);
+}
+
+test "Events: can register with namespace function" {
+    const testing = std.testing;
+    const allocator = testing.allocator;
+
+    // Define a event type
+    const Event = struct {
+        foo: i32,
+    };
+
+    // Define a callback that will handle the event.
+    const Callback = struct {
+        const Self = @This();
+        pub fn handleEvent(_: *anyopaque, _: *const Event) bool {
+            return true;
+        }
+    };
+
+    var events = try Events(Event).init(allocator, 8);
+    defer events.deinit(allocator);
+
+    events.register(@as(*anyopaque, @constCast(@ptrCast(&.{}))), Callback.handleEvent);
+    const handled = events.fire(&.{ .foo = 1 });
+
+    try testing.expectEqual(true, handled);
 }
